36a81ebe31012c4701ced2d696ef3473ed85e74b
Anton Kryukov <antohakriukow@gmail.com>
2022-05-24 07:58:21 +0300

    axios, react query, genres menu component

--- app/components/layout/Navigation/MenuContainer/genres/usePopularGenres.ts
+++ app/components/layout/Navigation/MenuContainer/genres/usePopularGenres.ts
@@ -1,1 +1,28 @@
-export const usePopularGenres = () => null
+import { useQuery } from 'react-query'
+
+import { GenreService } from '@/services/genre.service'
+
+import { getGenresUrl } from '@/config/api.config'
+
+import { IMenuItem } from '../menu.interface'
+
+export const usePopularGenres = () => {
+	const QueryData = useQuery(
+		'popular genre menu',
+		() => GenreService.getAll(),
+		{
+			select: ({ data }) =>
+				data
+					.map(
+						(genre): IMenuItem => ({
+							icon: genre.icon,
+							link: getGenresUrl(genre.slug),
+							title: genre.name,
+						})
+					)
+					.splice(0, 4),
+		}
+	)
+
+	return QueryData
+}
---
+++ app/shared/types/movie.types.ts
@@ -0,0 +1,9 @@
+import { TypeMaterialIconName } from './icon.types'
+
+export interface IGenre {
+	id: string
+	name: string
+	slug: string
+	description: string
+	icon: TypeMaterialIconName
+}
