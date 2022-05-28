3cc951a12351104bfadc51d8fc63afa3a49355be
Anton Kryukov <antohakriukow@gmail.com>
2022-05-28 15:12:17 +0300

    auth form

---
+++ app/components/ui/form-elements/Button.tsx
@@ -0,0 +1,14 @@
+import cn from 'classnames'
+import { FC } from 'react'
+
+import { IButton } from './form.interface'
+import styles from './form.module.scss'
+
+const Button: FC<IButton> = ({ children, className, ...props }) => {
+	return (
+		<button className={cn(styles.button, className)} {...props}>
+			{children}
+		</button>
+	)
+}
+export default Button
