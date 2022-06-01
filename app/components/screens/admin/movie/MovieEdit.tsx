e828ccffe562bac46b4de3ed09389b195ff35809
Anton Kryukov <antohakriukow@gmail.com>
2022-06-01 11:20:27 +0300

    MovieEdit component with custom react-select inputs

--- app/components/screens/admin/movie/MovieEdit.tsx
+++ app/components/screens/admin/movie/MovieEdit.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { stripHtml } from 'string-strip-html'
 
+import UploadField from '@/components/ui/form-elements/UploadField/UploadField'
+
 import SkeletonLoader from '@/ui/SkeletonLoader'
 import AdminNavigation from '@/ui/admin-navigation/AdminNavigation'
 import Button from '@/ui/form-elements/Button'
@@ -15,14 +17,13 @@ import Meta from '@/utils/meta/Meta'
 import { generateSlug } from '@/utils/string/generateSlug'
 
 import { IMovieEditInput } from './movie-edit.interface'
+import { useAdminActors } from './useAdminActors'
+import { useAdminGenres } from './useAdminGenres'
 import { useMovieEdit } from './useMovieEdit'
 
-const DynamicTextEditor = dynamic(
-	() => import('@/ui/form-elements/TextEditor'),
-	{
-		ssr: false,
-	}
-)
+const DynamicSelect = dynamic(() => import('@/ui/select/Select'), {
+	ssr: false,
+})
 
 const MovieEdit: FC = () => {
 	const {
@@ -37,6 +38,8 @@ const MovieEdit: FC = () => {
 	})
 
 	const { isLoading, onSubmit } = useMovieEdit(setValue)
+	const { data: genres, isLoading: isGenresLoading } = useAdminGenres()
+	const { data: actors, isLoading: isActorsLoading } = useAdminActors()
 
 	return (
 		<Meta title="Edit movie">
@@ -49,57 +52,147 @@ const MovieEdit: FC = () => {
 					<>
 						<div className={formStyles.fields}>
 							<Field
-								{...register('name', {
-									required: 'Name is required!',
+								{...register('title', {
+									required: 'Title is required!',
 								})}
-								placeholder="Name"
-								error={errors.name}
-								style={{ width: '31%' }}
+								placeholder="Title"
+								error={errors.title}
 							/>
 
-							<div style={{ width: '31%' }}>
-								<SlugField
-									register={register}
-									error={errors.slug}
-									generate={() => {
-										setValue('slug', generateSlug(getValues('name')))
-									}}
-								/>
-							</div>
+							<SlugField
+								register={register}
+								error={errors.slug}
+								generate={() => {
+									setValue('slug', generateSlug(getValues('title')))
+								}}
+							/>
 
 							<Field
-								{...register('icon', {
-									required: 'Icon is required!',
+								{...register('parameters.country', {
+									required: 'Country is required!',
 								})}
-								placeholder="Icon"
-								error={errors.icon}
+								placeholder="Country"
+								error={errors.parameters?.country}
 								style={{ width: '31%' }}
 							/>
-						</div>
 
-						<Controller
-							name="description"
-							control={control}
-							defaultValue=""
-							render={({
-								field: { value, onChange },
-								fieldState: { error },
-							}) => (
-								<DynamicTextEditor
-									onChange={onChange}
-									value={value}
-									error={error}
-									placeholder="Description"
-								/>
-							)}
-							rules={{
-								validate: {
-									required: (v) =>
-										(v && stripHtml(v).result.length > 0) ||
-										'Description is required!',
-								},
-							}}
-						/>
+							<Field
+								{...register('parameters.duration', {
+									required: 'Duration is required!',
+								})}
+								placeholder="Duration (min.)"
+								error={errors.parameters?.duration}
+								style={{ width: '31%' }}
+							/>
+							<Field
+								{...register('parameters.year', {
+									required: 'Year is required!',
+								})}
+								placeholder="Year"
+								error={errors.parameters?.year}
+								style={{ width: '31%' }}
+							/>
+
+							<Controller
+								name="genres"
+								control={control}
+								rules={{
+									required: 'Please select at least one genre!',
+								}}
+								render={({ field, fieldState: { error } }) => (
+									<DynamicSelect
+										error={error}
+										field={field}
+										placeholder="Genres"
+										options={genres || []}
+										isLoading={isGenresLoading}
+										isMulti
+									/>
+								)}
+							/>
+							<Controller
+								name="actors"
+								control={control}
+								rules={{
+									required: 'Please select at least one actor!',
+								}}
+								render={({ field, fieldState: { error } }) => (
+									<DynamicSelect
+										error={error}
+										field={field}
+										placeholder="Actors"
+										options={actors || []}
+										isLoading={isActorsLoading}
+										isMulti
+									/>
+								)}
+							/>
+
+							<Controller
+								name="poster"
+								control={control}
+								defaultValue=""
+								render={({
+									field: { value, onChange },
+									fieldState: { error },
+								}) => (
+									<UploadField
+										onChange={onChange}
+										value={value}
+										error={error}
+										folder="movies"
+										placeholder="Poster"
+									/>
+								)}
+								rules={{
+									required: 'Poster is required',
+								}}
+							/>
+
+							<Controller
+								name="bigPoster"
+								control={control}
+								defaultValue=""
+								render={({
+									field: { value, onChange },
+									fieldState: { error },
+								}) => (
+									<UploadField
+										onChange={onChange}
+										value={value}
+										error={error}
+										folder="movies"
+										placeholder="Big poster"
+									/>
+								)}
+								rules={{
+									required: 'Big poster is required',
+								}}
+							/>
+
+							<Controller
+								name="videoUrl"
+								control={control}
+								defaultValue=""
+								render={({
+									field: { value, onChange },
+									fieldState: { error },
+								}) => (
+									<UploadField
+										onChange={onChange}
+										value={value}
+										error={error}
+										folder="movies"
+										placeholder="Video"
+										style={{ marginTop: -25 }}
+										isNoImage
+									/>
+								)}
+								rules={{
+									required: 'Video is required',
+								}}
+							/>
+						</div>
 
 						<Button>Update</Button>
 					</>
