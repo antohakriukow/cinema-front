32390396a4736c89a1cc892a941eb6aa4c31e583
Anton Kryukov <antohakriukow@gmail.com>
2022-06-06 14:44:56 +0300

    film page

---
+++ app/components/screens/single-movie/SingleMovie.tsx
@@ -0,0 +1,37 @@
+import dynamic from 'next/dynamic'
+import { FC } from 'react'
+
+import { IGalleryItem } from '@/components/ui/gallery/gallery.interface'
+
+import Banner from '@/ui/banner/Banner'
+import Gallery from '@/ui/gallery/Gallery'
+import SubHeading from '@/ui/heading/SubHeading'
+
+import { IMovie } from '@/shared/types/movie.types'
+
+import Meta from '@/utils/meta/Meta'
+
+import Content from './Content/Content'
+
+const SingleMovie: FC<{ movie: IMovie; similarMovies: IGalleryItem[] }> = ({
+	movie,
+	similarMovies,
+}) => {
+	return (
+		<Meta title={movie.title} description={`Watch ${movie.title}`}>
+			<Banner
+				imagePath={movie.bigPoster}
+				Detail={() => <Content movie={movie} />}
+			/>
+
+			{/* Video player */}
+
+			<div className="mt-12">
+				<SubHeading title="Similar" />
+				<Gallery items={similarMovies} />
+			</div>
+		</Meta>
+	)
+}
+
+export default SingleMovie
