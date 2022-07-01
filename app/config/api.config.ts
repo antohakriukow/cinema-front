63c9471df092bda02ec7f2c168c0b8bbdb9b52e9
Anton Kryukov <antohakriukow@gmail.com>
2022-07-01 09:10:42 +0300

    api url changed in api.config

--- app/config/api.config.ts
+++ app/config/api.config.ts
@@ -1,5 +1,8 @@
-export const API_URL = `${process.env.APP_URL}/api`
-export const API_SERVER_URL = `${process.env.APP_SERVER_URL}/api`
+// export const API_URL = `${process.env.APP_URL}/api`
+// export const API_SERVER_URL = `${process.env.APP_SERVER_URL}/api`
+
+export const API_URL = 'http://api.cinema.kriukov.space/api'
+export const API_SERVER_URL = 'http://api.cinema.kriukov.space/api'
 
 export const getAuthUrl = (string: string) => `/auth${string}`
 export const getUsersUrl = (string: string) => `/users${string}`
